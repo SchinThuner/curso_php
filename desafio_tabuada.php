9067564d3aa047c482a9a25b6a03c2e0e43976f6
LUN4 <schin.thuner@gmail.com>
2022-03-31 23:43:21 -0300

    dois-primeiros-desafios

---
+++ desafio_tabuada.php
@@ -0,0 +1,7 @@
+<?php
+$numero = 6;
+for ($i=1; $i <= 10 ; $i++) { 
+    //$tabuada = $numero * $i;
+    //echo "$numero x $i = $tabuada".PHP_EOL;
+    echo "$numero x $i = ".$numero * $i.PHP_EOL;
+}
