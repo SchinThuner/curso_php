38c56ad289e265129e2467d43baff51a046fa58f
SchinThuner <61092834+SchinThuner@users.noreply.github.com>
2022-04-06 15:43:52 -0300

    Deletando arquivos duplicados

--- desafio_tabuada.php
+++
@@ -1,7 +0,0 @@
-<?php
-$numero = 6;
-for ($i=1; $i <= 10 ; $i++) { 
-    //$tabuada = $numero * $i;
-    //echo "$numero x $i = $tabuada".PHP_EOL;
-    echo "$numero x $i = ".$numero * $i.PHP_EOL;
-}
