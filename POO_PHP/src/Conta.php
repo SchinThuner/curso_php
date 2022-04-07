4be9233f21f2453b2e62fef6c445f1cf41a3a59d
4UR0R4 <schin.thuner@gmail.com>
2022-04-07 18:13:15 -0300

    metodos na classe

--- POO_PHP/src/Conta.php
+++ POO_PHP/src/Conta.php
@@ -6,4 +6,22 @@ class conta
  public string $cpfTitular;
  public string $nomeTitular;
  public float $saldo;
+
+ public function sacar($valorASacar)
+ {
+    if($valorASacar > $this->saldo){
+        echo "Você não pode efetuar o saque";
+    }else {
+        $this->saldo -= $valorASacar;
+    }
+ }
+
+ public function depositar($valorADepositar) : void
+ {
+     if ($valorADepositar > 0) {
+         $this->saldo += $valorADepositar;
+     }else {
+         echo "Você não pode efetuar deposito de valores negativos";
+     }
+ }
 }
