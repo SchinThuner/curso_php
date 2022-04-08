a701d251f2cce27a2053fcec127425738b727737
4UR0R4 <schin.thuner@gmail.com>
2022-04-08 13:49:04 -0300

    definindo um valor inicial em um atributo da classe

--- POO_PHP/src/Conta.php
+++ POO_PHP/src/Conta.php
@@ -5,7 +5,7 @@ class conta
  //definir dados da conta
  public string $cpfTitular;
  public string $nomeTitular;
- public float $saldo;
+ public float $saldo = 0;
 
  public function sacar($valorASacar)
  {
