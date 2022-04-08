37f8fde0d797d6ed62576e4eaf614455a80329e7
4UR0R4 <schin.thuner@gmail.com>
2022-04-08 15:01:59 -0300

    atualizando o arquivo informando o tipo do dado da saida

--- POO_PHP/src/Conta.php
+++ POO_PHP/src/Conta.php
@@ -7,21 +7,34 @@ class conta
  public string $nomeTitular;
  public float $saldo = 0;
 
- public function sacar($valorASacar)
+ public function sacar($valorASacar) : void
  {
     if($valorASacar > $this->saldo){
-        echo "Você não pode efetuar o saque";
-    }else {
-        $this->saldo -= $valorASacar;
-    }
+        echo "Saldo indisponível";
+        return;
+    }/*else { //utilizando a ideia de nao usar o else, pois é desnecessário se usado o return antes
+       $this->saldo -= $valorASacar;
+    }*/ 
+    $this->saldo -= $valorASacar;
  }
 
  public function depositar($valorADepositar) : void
  {
-     if ($valorADepositar > 0) {
-         $this->saldo += $valorADepositar;
-     }else {
+     if ($valorADepositar < 0) {
+        echo "Você não pode efetuar deposito de valores negativos";
+         return;
+     }/*else {
          echo "Você não pode efetuar deposito de valores negativos";
-     }
+     }*/
+     $this->saldo += $valorADepositar;
+ }
+ public function transferir(float $valorATransferir,conta $contaDestino) : void
+ {
+    if ($valorATransferir > $this->saldo) {
+        echo "Saldo indisponível";
+        return;
+    }
+    $this->sacar($valorATransferir);
+    $contaDestino->depositar($valorATransferir);
  }
 }
